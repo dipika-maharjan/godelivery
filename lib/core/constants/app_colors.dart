63490cc6e6489307749d96b0529b6a298c3f5b02
Dipika <maharjandipika49@gmail.com>
2026-09-01 13:18:13 +0545

    Set up initial project structure

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Brand
+  static const Color primary = Color(0xFFFFCC00);
+
+  // Basic
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // Backgrounds
+  static const Color background = Color(0xFFF8F8F8);
+  static const Color lightGrey = Color(0xFFF1F1F1);
+
+  // Text / borders
+  static const Color grey = Color(0xFF757575);
+  static const Color border = Color(0xFFE0E0E0);
+
+  // Status
+  static const Color success = Color(0xFF22C55E);
+  static const Color error = Color(0xFFE53935);
+}
