457f1fc38c7a2c19786de9d2f7e0f94073aaaba7
Dipika <maharjandipika49@gmail.com>
2026-09-02 14:26:12 +0545

    feat: Enhance home screen navigation with custom buttons and create action

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -13,57 +13,134 @@ class _HomeScreenState extends State<HomeScreen> {
   static const List<Widget> _screens = [
     _OrdersTab(),
     _ShipmentTab(),
-    _CreateTab(),
     _AccountTab(),
   ];
+
+  void _onCreatePressed() {
+    setState(() => _selectedIndex = 0);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF5F5F5),
-      body: IndexedStack(index: _selectedIndex, children: _screens),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(alpha: 0.05),
-              blurRadius: 8,
-              offset: const Offset(0, -2),
-            ),
-          ],
-        ),
-        child: SafeArea(
-          child: BottomNavigationBar(
-            currentIndex: _selectedIndex,
-            onTap: (index) => setState(() => _selectedIndex = index),
-            type: BottomNavigationBarType.fixed,
-            selectedItemColor: const Color(0xFFF3C91F),
-            unselectedItemColor: const Color(0xFF6B6B6B),
-            backgroundColor: Colors.white,
-            elevation: 0,
-            selectedFontSize: 12,
-            unselectedFontSize: 12,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.content_paste_rounded),
-                label: 'Order',
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: SafeArea(
+        top: false,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 16),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 76,
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(28),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.08),
+                        blurRadius: 12,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _NavButton(
+                        icon: Icons.file_copy_outlined,
+                        label: 'Order',
+                        isSelected: _selectedIndex == 0,
+                        onTap: () => setState(() => _selectedIndex = 0),
+                      ),
+                      _NavButton(
+                        icon: Icons.local_shipping_outlined,
+                        label: 'Shipment',
+                        isSelected: _selectedIndex == 1,
+                        onTap: () => setState(() => _selectedIndex = 1),
+                      ),
+                      _NavButton(
+                        icon: Icons.person_outline_rounded,
+                        label: 'Account',
+                        isSelected: _selectedIndex == 2,
+                        onTap: () => setState(() => _selectedIndex = 2),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.local_shipping_rounded),
-                label: 'Shipment',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add_circle_outline_rounded),
-                label: 'Create',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline_rounded),
-                label: 'Account',
+              const SizedBox(width: 12),
+              GestureDetector(
+                onTap: _onCreatePressed,
+                child: Container(
+                  width: 64,
+                  height: 64,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF111111),
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 32,
+                  ),
+                ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _NavButton extends StatelessWidget {
+  const _NavButton({
+    required this.icon,
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: isSelected ? const Color(0xFFF3C91F) : const Color(0xFF6E6E6E),
+            size: 26,
+          ),
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 11,
+              fontWeight: isSelected ? FontWeight.w700 : FontWeight.w500,
+              color: isSelected ? const Color(0xFFF3C91F) : const Color(0xFF6E6E6E),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -91,20 +168,6 @@ class _ShipmentTab extends StatelessWidget {
     return const Center(
       child: Text(
         'Shipment',
-        style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
-      ),
-    );
-  }
-}
-
-class _CreateTab extends StatelessWidget {
-  const _CreateTab();
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Create',
         style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
       ),
     );
